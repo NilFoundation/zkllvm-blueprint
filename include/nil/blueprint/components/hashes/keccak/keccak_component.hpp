75d015503a18464b56999bcc5e916181887f7fa0
Polina Chernyshova <chernyshova.pa@phystech.edu>
2023-07-27 20:08:14 +0000

    gates calculation #183

--- include/nil/blueprint/components/hashes/keccak/keccak_component.hpp
+++ include/nil/blueprint/components/hashes/keccak/keccak_component.hpp
@@ -44,16 +44,7 @@ namespace nil {
                 using component_type = plonk_component<BlueprintFieldType, ArithmetizationParams,
                                                        WitnessesAmount, 1, 0>;
                 using value_type = typename BlueprintFieldType::value_type;
-
-                std::size_t calculate_normalize_chunk_size(std::size_t num_rows, std::size_t i) {
-                    std::size_t res = 0;
-                    std::size_t power = i;
-                    while (power < num_rows) {
-                        ++res;
-                        power *= i;
-                    }
-                    return res;
-                }
+                using integral_type = typename BlueprintFieldType::integral_type;
 
                 std::size_t rows() const {
                     return 0;
@@ -62,74 +53,212 @@ namespace nil {
             public:
                 using var = typename component_type::var;
 
-                using per_chunk_component_type = keccak_per_chunk<crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType,
+                using round_component_type = keccak_round<crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType,
                                                                             ArithmetizationParams>,
                                             WitnessesAmount>;
-                std::array<round_component_type, 17> rounds;
+                std::vector<round_component_type> rounds;
+
+                using configuration = round_component_type::configuration;
+                std::vector<configuration> full_configuration;
 
                 const std::size_t lookup_rows;
                 const std::size_t lookup_columns;
-                const std::size_t normalize3_chunk_size;
-                const std::size_t normalize4_chunk_size;
-                const std::size_t normalize6_chunk_size;
+                
+                const std::size_t num_blocks;
+                const std::size_t num_round_calls;
+                const std::size_t num_configs;
 
-                constexpr static const bool needs_bonus_row = WitnessesAmount < 5;
+                const std::size_t pack_chunk_size;
+                const std::size_t pack_num_chunks;
+                const std::size_t pack_num_cells;
+
                 const std::size_t rows_amount;
                 constexpr static const std::size_t gates_amount = 1;
 
                 struct input_type {
-                    std::array<var, 25> inner_state;
-                    std::array<var, 17> padded_message_chunk;
-                    var round_constant;
+                    std::vector<var> message;
                 };
 
                 struct result_type {
                     std::array<var, 25> final_inner_state;
 
                     result_type(const keccak &component, std::size_t start_row_index) {
-                        std::pair<std::size_t, std::size_t>
-                            r_address = component.get_var_address(var_address::R_, start_row_index),
-                            q_address = component.get_var_address(var_address::Q, start_row_index);
-
-                        quotient = var(component.W(q_address.second), q_address.first);
-                        remainder = var(component.W(r_address.second), r_address.first);
+                        
                     }
                 };
 
+                integral_type pack(const integral_type& const_input) const {
+                    integral_type input = const_input;
+                    integral_type sparse_res = 0;
+                    integral_type power = 1;
+                    while (input > 0) {
+                        auto bit = input % 2;
+                        sparse_res += bit * power;
+                        power *= 8;
+                        input /= 2;
+                    }
+                    return sparse_res;
+                }
+
+                integral_type unpack(const integral_type& const_sparse_input) const {
+                    integral_type sparse_input = const_sparse_input;
+                    integral_type res = 0;
+                    integral_type power = 1;
+                    while (sparse_input > 0) {
+                        auto bit = sparse_input % 8;
+                        BOOST_ASSERT(bit * (1 - bit) == 0);
+                        res += bit * power;
+                        power *= 2;
+                        sparse_input /= 8;
+                    }
+                    return res;
+                }
+
+                configuration configure_pack_unpack(std::size_t row, std::size_t column) {
+                    // regular constraints:
+                    // input = input0 + input1 * 2^chunk_size + ... + inputk * 2^(k*chunk_size)
+                    // output = output0 + output1 * 2^chunk_size + ... + outputk * 2^(k*chunk_size)
+                    
+                    std::size_t last_row = row,
+                                last_column = column;
+                                
+                    std::vector<std::pair<std::size_t, std::size_t>> copy_from;
+                    std::vector<std::vector<std::pair<std::size_t, std::size_t>>> constraints;
+                    
+                    if (1 + column > limit) {
+                        copy_from.push_back({last_row + 1, 0});
+                    } else {
+                        copy_from.push_back({last_row + (last_column / WitnessesAmount),
+                                                        (last_column++) % WitnessesAmount});
+                    }
+                                    
+                    std::pair<std::size_t, std::size_t> cell_copy_to;
+                    std::size_t final_row = (column + num_cells - 1) / WitnessesAmount + row;
+                    if (final_row == copy_from[0].first) {
+                        cell_copy_to = {final_row, copy_from.back().second + 1};
+                    } else {
+                        cell_copy_to = {final_row, 0};
+                    }
+                    
+                    std::vector<std::pair<std::size_t, std::size_t>> cells;
+                    if (1 + column > limit) {
+                        for (int i = column; i < WitnessesAmount; ++i) {
+                            cells.push_back({row, i});
+                        }
+                        std::size_t cells_left = num_cells - WitnessesAmount + column;
+                        std::size_t cur_row = row + 1,
+                                    cur_column = 1;
+                        while (cur_column < cells_left) {
+                            if (cur_column % WitnessesAmount == cell_copy_to.second && (cur_row + (cur_column / WitnessesAmount) == cell_copy_to.first)) {
+                                cur_column++;
+                                continue;
+                            }
+                            cells.push_back({cur_row + (cur_column / WitnessesAmount), (cur_column++) % WitnessesAmount});
+                        }
+                    } else {
+                        std::size_t cur_row = row,
+                                    cur_column = column + 1;
+                        while (cur_column - column < num_cells) {
+                            if (cur_column % WitnessesAmount == cell_copy_to.second && (cur_row + (cur_column / WitnessesAmount) == cell_copy_to.first)) {
+                                cur_column++;
+                                continue;
+                            }
+                            cells.push_back({cur_row + (cur_column / WitnessesAmount), (cur_column++) % WitnessesAmount});
+                        }
+                    }                
+                    std::size_t cell_index = 0;
+                    
+                    std::vector<std::vector<std::pair<std::size_t, std::size_t>>> lookups(num_chunks, std::vector<std::pair<std::size_t, std::size_t>>());
+                        
+                    constraints.push_back({copy_from[0]});
+                    constraints.push_back({cell_copy_to});
+                    for (std::size_t i = 0; i < 2; ++i) {
+                        for (std::size_t j = 0; j < num_chunks; ++j) {
+                            constraints[i].push_back(cells[cell_index++]);
+                            lookups[j].push_back(constraints[i].back());
+                        }
+                    }
+                    
+                    last_column = cells.back().second + 1;
+                    last_row = cells.back().first + (last_column / WitnessesAmount);
+                    last_column %= WitnessesAmount;
+                    
+                    
+                    return configuration({last_row, last_column}, copy_from, constraints, lookups, cell_copy_to);
+                }
+
+                std::vector<configuration> configure_all(const std::size_t num_configs,
+                                                         const std::size_t num_round_calls) {
+                    std::vector<configuration> result;
+                    std::size_t round_rows = round_component_type::rows_amount;
+
+                    std::size_t row = 0,
+                                column = 0;
+                    // padding
+
+                    // rounds
+                    for (std::size_t index = 0; index < num_round_calls; ++index) {
+                        for (std::size_t i = 0; i < 17; ++i) {
+                            result.push_back(configure_pack_unpack(row, column));
+                            row = result[i].last_row;
+                            column = result[i].last_column;
+                        }
+                        if (column > 0) {
+                            column = 0;
+                            row += 1 + 24 * round_rows;
+                        } else {
+                            row += 24 * round_rows;
+                        }
+                    }
+
+                    // finalization
+                    for (std::size_t i = 0; i < 5; ++i) {
+                        result.push_back(configure_pack_unpack(row, column));
+                        row = result[i].last_row;
+                        column = result[i].last_column;
+                    }
+
+                    return result;
+                }
+
                 #define __keccak_init_macro(witness, constant, public_input, \
-                                                        lookup_rows_, lookup_columns_) \
+                                                        lookup_rows_, lookup_columns_, num_blocks_) \
                     lookup_rows(lookup_rows_), \
                     lookup_columns(lookup_columns_), \
-                    normalize3_chunk_size(calculate_normalize_chunk_size(lookup_rows_, 3)), \
-                    normalize4_chunk_size(calculate_normalize_chunk_size(lookup_rows_, 4)), \
-                    normalize6_chunk_size(calculate_normalize_chunk_size(lookup_rows_, 6)), \
-                    range_checks(range_check_amount, \
-                                 range_check_component_type(witness, constant, public_input, bits_amount_)), \
+                    num_blocks(num_blocks_), \
+                    num_round_calls(), \
+                    rounds(num_round_calls * 24, \
+                            round_component_type(witness, constant, public_input, lookup_rows_, lookup_columns_)), \
+                    num_configs(), \
+                    full_configuration(configure_all(num_configs, num_round_calls)), \
                     rows_amount(rows())
 
                 template<typename ContainerType>
-                division_remainder(ContainerType witness, std::size_t bits_amount_, bool check_inputs_) :
+                keccak(ContainerType witness, std::size_t lookup_rows_,
+                        std::size_t lookup_columns_, std::size_t num_blocks_) :
                     component_type(witness, {}, {}),
-                    __division_remainder_init_macro(witness, {}, {}, bits_amount_, check_inputs_) {};
+                    __keccak_init_macro(witness, {}, {}, lookup_rows_, lookup_columns_, num_blocks_) {};
 
 
                 template<typename WitnessContainerType, typename ConstantContainerType,
                          typename PublicInputContainerType>
-                division_remainder(WitnessContainerType witness, ConstantContainerType constant,
+                keccak(WitnessContainerType witness, ConstantContainerType constant,
                                    PublicInputContainerType public_input,
-                                   std::size_t bits_amount_, bool check_inputs_):
+                                   std::size_t lookup_rows_,
+                                   std::size_t lookup_columns_,
+                                   std::size_t num_blocks_):
                     component_type(witness, constant, public_input),
-                    __division_remainder_init_macro(witness, constant, public_input, bits_amount_, check_inputs_) {};
+                    __keccak_init_macro(witness, constant, public_input, lookup_rows_, lookup_columns_, num_blocks_) {};
 
-                division_remainder(
+                keccak(
                     std::initializer_list<typename component_type::witness_container_type::value_type> witnesses,
                     std::initializer_list<typename component_type::constant_container_type::value_type> constants,
                     std::initializer_list<typename component_type::public_input_container_type::value_type>
                         public_inputs,
-                    std::size_t bits_amount_, bool check_inputs_) :
+                        std::size_t lookup_rows_, std::size_t lookup_columns_, std::size_t num_blocks_) :
                         component_type(witnesses, constants, public_inputs),
-                        __division_remainder_init_macro(witnesses, constants, public_inputs,
-                                                        bits_amount_, check_inputs_)
+                        __keccak_init_macro(witnesses, constants, public_inputs,
+                                            lookup_rows_, lookup_columns_, num_blocks_)
                 {};
 
                 #undef __keccak_init_macro
@@ -161,7 +290,6 @@ namespace nil {
                 using component_type = keccak_component<BlueprintFieldType, ArithmetizationParams,
                                                                 WitnessesAmount>;
                 using var = typename component_type::var;
-                using var_address = typename component_type::var_address;
                 using constraint_type = crypto3::zk::snark::plonk_constraint<BlueprintFieldType>;
                 using gate_type = typename crypto3::zk::snark::plonk_gate<BlueprintFieldType, constraint_type>;
                 using value_type = typename BlueprintFieldType::value_type;
@@ -187,7 +315,6 @@ namespace nil {
                 using component_type = keccak_component<BlueprintFieldType, ArithmetizationParams,
                                                                 WitnessesAmount>;
                 using var = typename component_type::var;
-                using var_address = typename component_type::var_address;
                 std::uint32_t row = start_row_index;
             }
 
