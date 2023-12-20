856e7eb69c0e44615e2aaa51446ee2814092ccec
ayashunsky <alexey.yashunsky@gmail.com>
2023-12-20 15:07:07 +0200

    BN exponentiation

--- test/algebra/pairing/weierstrass/plonk/bn_pairing.cpp
+++ test/algebra/pairing/weierstrass/plonk/bn_pairing.cpp
@@ -22,10 +22,11 @@
 // SOFTWARE.
 //---------------------------------------------------------------------------//
 
-#define BOOST_TEST_MODULE blueprint_plonk_fields_non_native_fp12_test
+#define BOOST_TEST_MODULE blueprint_plonk_pairing_bn
 
 #include <boost/test/unit_test.hpp>
 
+#include <nil/crypto3/algebra/curves/bls12.hpp>
 #include <nil/crypto3/algebra/fields/bls12/base_field.hpp>
 #include <nil/crypto3/algebra/fields/arithmetic_params/bls12.hpp>
 
@@ -39,7 +40,8 @@
 
 #include <nil/blueprint/blueprint/plonk/circuit.hpp>
 #include <nil/blueprint/blueprint/plonk/assignment.hpp>
-#include <nil/blueprint/components/algebra/fields/plonk/non_native/fp12_fixed_power.hpp>
+
+#include <nil/blueprint/components/algebra/pairing/weierstrass/plonk/bn_exponentiation.hpp>
 
 #include "../../../../test_plonk_component.hpp"
 
@@ -47,11 +49,11 @@ using namespace nil;
 using namespace blueprint::components::detail;
 
 template <typename FieldType>
-void test_fp12_fixed_power(std::vector<typename FieldType::value_type> public_input, unsigned long long Power) {
+void test_bn_exponentiation(std::vector<typename FieldType::value_type> public_input, unsigned long long t) {
     constexpr std::size_t WitnessColumns = 12;
     constexpr std::size_t PublicInputColumns = 1;
     constexpr std::size_t ConstantColumns = 0;
-    constexpr std::size_t SelectorColumns = 4;
+    constexpr std::size_t SelectorColumns = 8;
 
     using ArithmetizationParams =
         crypto3::zk::snark::plonk_arithmetization_params<WitnessColumns, PublicInputColumns, ConstantColumns, SelectorColumns>;
@@ -63,7 +65,7 @@ void test_fp12_fixed_power(std::vector<typename FieldType::value_type> public_in
     using value_type = typename FieldType::value_type;
     using var = crypto3::zk::snark::plonk_variable<value_type>;
 
-    using component_type = blueprint::components::fp12_fixed_power<ArithmetizationType, FieldType>;
+    using component_type = blueprint::components::bn_exponentiation<ArithmetizationType, FieldType>;
 
     typename component_type::input_type instance_input;
     typename std::array<value_type,12> X;
@@ -77,8 +79,17 @@ void test_fp12_fixed_power(std::vector<typename FieldType::value_type> public_in
     using policy_type_fp12 = crypto3::algebra::fields::fp12_2over3over2<FieldType>;
     using fp12_element = typename policy_type_fp12::value_type;
 
-    fp12_element e1 = fp12_element({ {X[0],X[1]}, {X[2],X[3]}, {X[4],X[5]} }, { {X[6],X[7]}, {X[8],X[9]}, {X[10],X[11]} }),
-                 e = e1.pow(Power); // fp12 power raising
+    typename FieldType::integral_type field_p = FieldType::modulus;
+
+    fp12_element e0 = fp12_element({ {X[0],X[1]}, {X[2],X[3]}, {X[4],X[5]} }, { {X[6],X[7]}, {X[8],X[9]}, {X[10],X[11]} }),
+                 e = e0.unitary_inversed() * e0.inversed(), // e0^{p^6 - 1}
+                 f = e.pow(field_p).pow(field_p) * e, // (e0^{p^6 - 1})^{p^2 + 1}
+                 a = f.pow(t).pow(6) * f.pow(5).inversed(),
+                 b = a.pow(field_p + 1);
+
+    e = b * f.pow(field_p).pow(2) * f.pow(field_p).pow(field_p);
+    e = e.pow(t).pow(t).pow(6) * e;
+    e = e * f.pow(field_p).pow(field_p).pow(field_p) * b * f.pow(field_p + 1).pow(9) * a * f.pow(4);
 
     expected_res = {
        e.data[0].data[0].data[0], e.data[0].data[0].data[1],
@@ -92,7 +103,7 @@ void test_fp12_fixed_power(std::vector<typename FieldType::value_type> public_in
     auto result_check = [&expected_res, public_input](AssignmentType &assignment,
             typename component_type::result_type &real_res) {
             #ifdef BLUEPRINT_PLONK_PROFILING_ENABLED
-            std::cout << "Fp12 small power res vs output\n";
+            std::cout << "BN exponentiation expected res vs output\n";
             for(std::size_t i = 0; i < 12; i++) {
                 std::cout << std::dec << expected_res[i].data << " =? " << var_value(assignment, real_res.output[i]).data << "\n";
             }
@@ -102,21 +113,20 @@ void test_fp12_fixed_power(std::vector<typename FieldType::value_type> public_in
             }
     };
 
-    component_type component_instance({0,1,2,3,4,5,6,7,8,9,10,11}, // witnesses
-                                      {},  // constants
-                                      {}, // public inputs
-                                      Power);
-
+    component_type component_instance({0,1,2,3,4,5,6,7,8,9,10,11}, {}, {}, t);
     nil::crypto3::test_component<component_type, FieldType, ArithmetizationParams, hash_type, Lambda> (
-           component_instance, public_input, result_check, instance_input, nil::crypto3::detail::connectedness_check_type::STRONG, Power);
+           component_instance, public_input, result_check, instance_input, nil::crypto3::detail::connectedness_check_type::STRONG, t);
 }
 
-static const std::size_t random_tests_amount = 10;
+static const std::size_t random_tests_amount = 5;
 
 BOOST_AUTO_TEST_SUITE(blueprint_plonk_test_suite)
 
-BOOST_AUTO_TEST_CASE(blueprint_plonk_fields_non_native_fp12_test) {
-    using field_type = typename crypto3::algebra::fields::bls12_fq<381>;
+BOOST_AUTO_TEST_CASE(blueprint_plonk_pairing_bn) {
+    using curve_type = crypto3::algebra::curves::bls12_381;
+    using g2_group_type = typename curve_type::g2_type<>;
+    using base_field_value = curve_type::base_field_type::value_type;
+    using field_type = typename curve_type::g2_type<>::field_type::base_field_type; //typename crypto3::algebra::fields::bls12_fq<381>;
 
     nil::crypto3::random::algebraic_engine<field_type> generate_random;
     boost::random::mt19937 seed_seq;
@@ -124,21 +134,15 @@ BOOST_AUTO_TEST_CASE(blueprint_plonk_fields_non_native_fp12_test) {
 
     for(std::size_t i = 0; i < random_tests_amount; i++) {
         std::cout << "Random test # " << (i+1) << "\n";
+
         std::vector<field_type::value_type> x = {};
 
         for(std::size_t j = 0; j < 12; j++) {
             x.push_back(generate_random());
         }
-        unsigned long long Power = 0xD201000000010000;
-        test_fp12_fixed_power<field_type>(x,Power);
 
-        Power = 0x460055555555aaab;
-        test_fp12_fixed_power<field_type>(x,Power);
-
-        Power = 4965661367192848881;
-        test_fp12_fixed_power<field_type>(x,Power);
+        test_bn_exponentiation<field_type>(x,4965661367192848881);
     }
-
 }
 
 BOOST_AUTO_TEST_SUITE_END()
